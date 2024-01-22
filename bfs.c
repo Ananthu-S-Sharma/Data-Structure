6242f348b3e57162de1b5a59cb4a9f4852b1fbf0
Ananthu-S-Sharma <146819078+Ananthu-S-Sharma@users.noreply.github.com>
2024-01-22 11:08:41 +0530

    Add files via upload

--- bfs.c
+++ bfs.c
@@ -1,51 +1,30 @@
-#include <stdio.h>
-
-void BFS(int);
-int graph[10][10], visited[10], total;
-
-int main() {
-    int i, j;
-
-    printf("\nEnter the total number of vertices in graph\n");
-    scanf("%d", &total);
-
-   
-    printf("\nEnter the adjacency matrix\n");
-    for (i = 0; i < total; i++) {
-        for (j = 0; j < total; j++) {
-            scanf("%d", &graph[i][j]);
-        }
-    }
-
-    for (i = 0; i < total; i++) {
-        visited[i] = 0;
-    }
-
-    printf("\nBFS traversal is \n");
-    BFS(0);
-
-    return 0;
+#include<stdio.h>
+int graph[10][10],visited[10],total;
+void bfs(int vertex)
+{
+	int j;
+	printf("%d\t",vertex);
+	visited[vertex]=1;
+	for(j=0;j<total;j++)
+	{
+	  if(!visited[j]&&graph[vertex][j])
+		{ bfs(j);
+			}
+		}
 }
-
-void BFS(int startVertex) {
-    int queue[10], front = -1, rear = -1, i, j;
-
-    
-    queue[++rear] = startVertex;
-    visited[startVertex] = 1;
-
-    while (front != rear) {
-        
-        int currentVertex = queue[++front];
-        printf("%d\t", currentVertex);
-
-        
-        for (j = 0; j < total; j++) {
-            if (!visited[j] && graph[currentVertex][j] == 1) {
-                
-                queue[++rear] = j;
-                visited[j] = 1;
-            }
-        }
-    }
+int main()
+{
+	int i,j;
+	printf("enter the vertices");
+	scanf("%d",&total);
+	printf("enter the adjacency mateix");
+	for(i=0;i<total;i++)
+	for(j=0;j<total;j++)
+	  scanf("%d",&graph[i][j]);
+	for(i=0;i<total;i++)
+	  visited[i]=0;
+	printf("bfs traversal are");
+	bfs(1);
+	
+   	return 0;
 }
